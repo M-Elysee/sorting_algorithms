a1890d32b56085e4a0f1192839664d11bddce23c
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-07-25 17:15:47 +0000

    0x1B. C - Sorting algorithms & Big O_ debug_2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,8 +7,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *head = *list, *h, *temp, *h_prev, *tp;
 
-
-	if (!list || !*list || !(*list)->next)
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
 		return;
 	while (head)
 	{
